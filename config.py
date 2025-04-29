55f11acff63a76e3dcab07360df56eb9fd90b96e
carlostoek <carlostoek@gmail.com>
2025-04-29 12:22:11 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN no está definido en el archivo .env")
+if ADMIN_ID is None:
+    raise ValueError("ADMIN_ID no está definido en el archivo .env")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL no está definido en el archivo .env")
