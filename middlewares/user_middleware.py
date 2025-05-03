dd461d64fadaec7685abd35055c598486932ea8b
carlostoek <carlostoek@gmail.com>
2025-05-03 05:17:16 -0600

    Update user_middleware.py

--- middlewares/user_middleware.py
+++ middlewares/user_middleware.py
@@ -2,6 +2,7 @@ from typing import Callable, Dict, Any, Awaitable
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject, User
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from database import models
 from database.database import get_db
 
@@ -13,13 +14,14 @@ class UserMiddleware(BaseMiddleware):
         data: Dict[str, Any]
     ) -> Any:
         user: User = event.from_user
-        async for db in get_db():  # Usamos async for para iterar sobre el async generator
-            user_exists = await db.query(models.Usuario).filter(models.Usuario.user_id == user.id).first()
+        async for db in get_db():
+            result = await db.execute(select(models.Usuario).where(models.Usuario.user_id == user.id))
+            user_exists = result.scalar_one_or_none()
             if not user_exists:
                 new_user = models.Usuario(user_id=user.id, username=user.username)
                 db.add(new_user)
                 await db.commit()
                 print(f"Nuevo usuario registrado: {user.id} - {user.username}")
-            data["db"] = db  # Pasar la sesión de la base de datos a los handlers
+            data["db"] = db
             result = await handler(event, data)
             return result
